3ba37d091a35f078cb3c6d1e59f4d4bb5ed5dae6
Yannick <yhueftle@gmail.com>
2017-02-27 12:57:24 -0700

    styling

--- controllers.js
+++ controllers.js
@@ -4,15 +4,20 @@ app.controller('RequestPokemonController', ['$scope', 'pokeApi', '$http', functi
 	var self = this;
 	self.pokeApi=pokeApi;
 	self.pokemonName;
-	self.getPokemon = function(pokemonName){
+	self.getPokemon = function(){
 		self.pokemonObj = null;
 		self.image = null;
-		var promise = pokeApi.requestPokemon(pokemonName).then(function(response){
+		var promise = pokeApi.requestPokemon(self.pokemonName).then(function(response){
 			self.pokemonObj = response;
 			self.image = self.pokemonObj.sprites.front_default;
 		}, function(response){
 			self.pokemonObj = null;
 			self.notfound = 'pokemon not found';
 		});
+	};
+	self.enter = function(keyEvent){
+		if(keyEvent.which === 13){
+			self.getPokemon(self.pokemonName);
+		};
 	};
 }]);
